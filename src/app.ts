51944b140000e2c3b93fdaec4448be02fe23e09b
Timur Karimov <tkarimov@academic.rrc.ca>
2026-03-15 21:03:09 -0500

    update app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
 import express from "express";
+import fs from "fs";
+import morgan from "morgan";
 import { healthRoutes } from "./api/v1/routes/healthRoutes";
-import { eventRoutes } from "./api/v1/routes/eventRoutes";
+import { loanApplicationRoutes } from "./api/v1/routes/loanApplicationRoutes";
+import { userRoutes } from "./api/v1/routes/userRoutes";
+import { adminRoutes } from "./api/v1/routes/adminRoutes";
+import errorHandler from "./api/v1/middleware/errorHandler";
+import { getAccessLogFilePath } from "./api/v1/utils/loggingUtils";
 
 export const app = express();
 
+const accessLogStream = fs.createWriteStream(getAccessLogFilePath(), {
+  flags: "a"
+});
+
 app.use(express.json());
+app.use(morgan("combined", { stream: accessLogStream }));
+app.use(morgan("dev"));
 
 app.use("/api/v1", healthRoutes);
-app.use("/api/v1", eventRoutes);
+app.use("/api/v1", loanApplicationRoutes);
+app.use("/api/v1", userRoutes);
+app.use("/api/v1", adminRoutes);
+
+app.use(errorHandler);
