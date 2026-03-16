ff3c60416f75ea12bd343221898da9a8fa0d4af7
Timur Karimov <tkarimov@academic.rrc.ca>
2026-03-15 20:24:05 -0500

    add user  controller

---
+++ src/api/v1/controllers/userController.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import { auth } from "../../../config/firebaseConfig";
+import { HTTP_STATUS } from "../../../constants/httpConstants";
+import { BadRequestError, NotFoundError } from "../errors/errors";
+
+export async function getUserDetailsController(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    const userId: string | undefined = request.params.uid;
+
+    if (!userId) {
+      throw new BadRequestError("User id is required", "USER_ID_REQUIRED");
+    }
+
+    const userRecord = await auth.getUser(userId);
+
+    response.status(HTTP_STATUS.OK).json({
+      success: true,
+      data: {
+        uid: userRecord.uid,
+        email: userRecord.email,
+        disabled: userRecord.disabled,
+        customClaims: userRecord.customClaims ?? {}
+      }
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error && error.message.includes("There is no user")) {
+      next(new NotFoundError("User not found", "USER_NOT_FOUND"));
+      return;
+    }
+
+    next(error);
+  }
+}
