cc4e3bb83095f0d7eb6178cd1a44536f832db56b
Timur Karimov <tkarimov@academic.rrc.ca>
2026-03-15 21:55:57 -0500

    update authorization options

--- src/api/v1/models/authorizationOptions.ts
+++ src/api/v1/models/authorizationOptions.ts
@@ -1,4 +1,4 @@
-export interface AuthorizationOptions{
-    hasRole: Array <"admin" | "manager" | "user">
-    allowSameUser?: boolean;
+export interface AuthorizationOptions {
+  hasRole: Array<"user" | "manager" | "admin">;
+  allowSameUser?: boolean;
 }
--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,9 +1,15 @@
 import { cert, initializeApp, ServiceAccount } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import * as serviceAccount from "../../assignment-cca09-firebase-adminsdk-fbsvc-75ce29446a.json";
+import serviceAccountJson from "../../assignment-cca09-firebase-adminsdk-fbsvc-75ce29446a.json";
+
+const serviceAccount: ServiceAccount = {
+  projectId: serviceAccountJson.project_id,
+  clientEmail: serviceAccountJson.client_email,
+  privateKey: serviceAccountJson.private_key
+};
 
 initializeApp({
-  credential: cert(serviceAccount as ServiceAccount)
+  credential: cert(serviceAccount)
 });
 
 export const auth = getAuth();
